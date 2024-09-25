ec78edaa23d8d4952a2bab5e4d7eecb6b6ed6251
BaekGeon Kim <ooi0929.kim@gmail.com>
2024-09-25 11:42:17 +0900

    [Add] Node에 mysql 연동

---
+++ 3weeks/nodejs/mysql.js
@@ -0,0 +1,23 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'webdb2024',
+    }
+);
+
+connection.query(
+    'SELECT * from topic', 
+    (error, results, fields)=> {
+        if (error) {
+            console.log(error);
+        }
+        console.log(results[0].title);
+        console.log(results[0].descrpt);
+    }
+);
+
+connection.end();
