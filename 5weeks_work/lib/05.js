171a8b6a0dac140c17d630b05781ebe15af0479c
ooi0929 <ooi0929.kim@gmail.com>
2024-10-09 09:07:35 +0900

    [Add] 5주차 과제 등록

--- 5weeks_work/lib/05.js
+++ 5weeks_work/lib/05.js
@@ -3,21 +3,190 @@ var qs = require('querystring');
 
 module.exports = {
     home: (req, res) => {
+        db.query(
+            'SELECT * FROM schedule05',
+            (err, schedules) => {
+                if (err) throw err;
 
+                var menu = '<a href="/create">일정생성</a>';
+                var detail = '<h4>상세일정</h4><p>제목을 클릭하시면 여기에 상세 일정 내용이 나옵니다.</p>';
+
+                var context = {
+                    schedules: schedules,
+                    menu: menu,
+                    detail: detail,
+                };
+
+                res.render('05', context, (err, html)=> res.end(html));
+            }
+        );
     },
     page: (req, res) => {
+        var id = req.params.pageId;
 
+        db.query(
+            'SELECT * FROM schedule05',
+            (err, schedules) => {
+                if (err) throw err;
+
+                db.query(
+                    `SELECT * FROM schedule05 WHERE id = ${id}`,
+                    (err2, schedule) => {
+                        if (err2) throw err2;
+
+                        var menu = `
+                            <a href="/create">일정생성</a>&nbsp;&nbsp;
+                            <a href="/update/${schedule[0].id}">일정수정</a>&nbsp;&nbsp;
+                            <a href="#">일정삭제</a>`;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 `89 `
+
+                        var detail = `
+                            <h4>${schedule[0].title}</h4>
+                            <p>일정 시작일 : ${schedule[0].start}</p>
+                            <p>일정 종료일 : ${schedule[0].end}</p>
+                            <p>상제 일정: ${schedule[0].content}</p>
+                            `;
+
+                        var context = {
+                            schedules: schedules,
+                            menu: menu,
+                            detail: detail,
+                        }
+
+                        res.render('05', context, (err, html)=> res.end(html));
+                    }
+                );
+            }
+        );
     },
     create: (req, res) => {
+        db.query(
+            'SELECT * FROM schedule05',
+            (err, schedules) => {
+                if (err) throw err;
 
+                var menu = '<a href="/create">일정생성</a>';
+                var detail = `
+                    <form action="/create_process" method="post">
+                        <p><input type="text" name="title" placeholder="일정제목"></input></p>
+                        <p><input type="text" name="start" placeholder="20241231" length="8"></input></p>
+                        <p><input type="text" name="end" placeholder="20241231" length="8"></input></p>
+                        <p><textarea name="content" placeholder="내용"></textarea></p>
+                        <p><input type="submit"></input></p>
+                    </form>
+                `;
+                
+                var context = {
+                    schedules: schedules,
+                    menu: menu,
+                    detail: detail,
+                };
+
+                res.render('05', context, (err, html)=> res.end(html));
+            }
+        );
     },
     create_process: (req, res) => {
+        var body = '';
+        
+        req.on(
+            'data',
+            (data) => {
+                body += data;
+            }
+        );
 
+        req.on(
+            'end',
+            () => {
+                var post = qs.parse(body);
+                
+                db.query(
+                    'INSERT INTO schedule05 (title, start, end, content, created) VALUES(?, ?, ?, ?, Now())',
+                    [post.title, post.start, post.end, post.content], (err, result) => {
+                        if (err) throw err;
+
+                        res.writeHead(
+                            302,
+                            { Location: `/page/${result.insertId}`}
+                        );
+
+                        res.end();
+                    }
+                );
+            }
+        );
     },
     update: (req, res) => {
+        var id = req.params.pageId;
 
+        db.query(
+            'SELECT * FROM schedule05',
+            (err, schedules) => {
+                if (err) throw err;
+
+                db.query(
+                    `SELECT * FROM schedule05 WHERE id = ${id}`,
+                    (err2, schedule) => {
+                        if (err2) throw err2;
+
+                        var menu = `
+                            <a href="/create">일정생성</a>&nbsp;&nbsp;
+                            <a href="/update/${schedule[0].id}">일정수정</a>&nbsp;&nbsp;
+                            <a href="#">일정삭제</a>
+                        `;
+                        
+                        var detail = `
+                            <form action="/update_process" method="post">
+                                <input type="hidden" name="id" value="${schedule[0].id}">
+                                <p><input type="text" name="title" placeholder="일정제목" value="${schedule[0].title}"></input></p>
+                                <p><input type="text" name="start" placeholder="20241231" length="8" value="${schedule[0].start}"></input></p>
+                                <p><input type="text" name="end" placeholder="20241231" length="8" value="${schedule[0].end}"></input></p>
+                                <p><textarea name="content" placeholder="내용">${schedule[0].content}</textarea></p>
+                                <p><input type="submit"></input></p>
+                            </form>
+                        `;
+
+                        var context = {
+                            schedules: schedules,
+                            menu: menu,
+                            detail: detail,
+                        };
+
+                        res.render('05', context, (err, html)=> res.end(html));
+                    }
+                );
+            }
+        );
     },
     update_process: (req, res) => {
+        var body = '';
 
+        req.on(
+            'data',
+            (data) => {
+                body += data;
+            }
+        );
+
+        req.on(
+            'end',
+            () => {
+                var post = qs.parse(body);
+
+                db.query(
+                    'UPDATE schedule05 SET title = ?, start = ?, end = ?, content = ? where id = ?',
+                    [post.title, post.start, post.end, post.content, post.id], (err, result) => {
+                        if (err) throw err;
+
+                        res.writeHead(
+                            302,
+                            { Location: `/page/${post.id}` }
+                        );
+
+                        res.end();
+                    }
+                );
+            }
+        );
     },
 }
--- 5weeks_work/main.js
+++ 5weeks_work/main.js
@@ -5,11 +5,36 @@ const app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-var todo = require('./lib/05');
+var schedule = require('./lib/05');
 
 app.get(
     '/',
-    (req, res) => todo.home(req, res),
+    (req, res) => schedule.home(req, res),
+);
+
+app.get(
+    '/page/:pageId',
+    (req, res) => schedule.page(req, res),
+);
+
+app.get(
+    '/create',
+    (req, res) => schedule.create(req, res),
+);
+
+app.post(
+    '/create_process',
+    (req, res) => schedule.create_process(req, res),
+);
+
+app.get(
+    '/update/:pageId',
+    (req, res) => schedule.update(req, res),
+);
+
+app.post(
+    '/update_process',
+    (req, res) => schedule.update_process(req, res),
 );
 
 app.get(
